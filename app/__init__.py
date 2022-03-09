8c4c5b203c3c7a4bc7cfcc8308a8abe7c71e901e
Cameron Leblanc <98185428+camleblanc3@users.noreply.github.com>
2022-03-09 13:08:06 -0700

    finished backbone setuo

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,11 @@
-print('hello')
+#import everything needed here
+from flask import Flask 
+
+#from config file you can import the config file
+from config import Config
+
+#define Flask object
+app = Flask(__name__)
+
+#tell app how it should be configured -- on config.py file
+app.config.from_object(Config)
---
+++ config.py
@@ -0,0 +1,16 @@
+#set up/ organize applications configuration
+
+#import os package
+import os
+
+#set up base directory of entire application
+basedir = os.path.abspath(os.path.dirname(__name__))
+
+#CONFIG variables set up
+class Config:
+    """
+    set configuration variables for entire flask app
+    """
+    FLASK_APP = os.environ.get('FLASK_APP')
+    FLASK_ENV = os.environ.get('FLASK_ENV')
+    SECRET_KEY = os.environ.get('SECRET_KEY')

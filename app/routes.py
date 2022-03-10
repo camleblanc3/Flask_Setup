af59d3321de9e17befdc58228b4a353fe1830579
Cameron Leblanc <98185428+camleblanc3@users.noreply.github.com>
2022-03-10 13:58:00 -0700

    jinja

--- app/routes.py
+++ app/routes.py
@@ -8,7 +8,9 @@ from flask import render_template
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    westas = ['lebron james', 'stephen curry', 'andrew wiggins', 'ja morant', 'nikola jokic', 'devin booker', 'rudy gobert', 'chris paul', 'draymond green', 'donovan mitchell', 'luka doncic', ' dejounte murray', 'karl-anthony towns']
+    eastas = ['kevin durant', 'trae young', 'jayson tatum', 'joel embiid', 'demar derozan', 'giannis antetokounmpo', 'lamelo ball', 'darius garland', 'james harden', 'zach lavine', 'fred vanvleet', 'jimmy butler', 'khris middleton', 'jarrett allen']
+    return render_template('index.html', westas = westas, eastas = eastas)
 
 
 @app.route('/page')

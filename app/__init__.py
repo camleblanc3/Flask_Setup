898123ad785f227f127174ce2a10099a275705cd
Cameron Leblanc <98185428+camleblanc3@users.noreply.github.com>
2022-03-15 11:00:08 -0600

    api form

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,10 @@ from flask import Flask
 #from config file you can import the config file
 from config import Config
 
+#import for database stuff
+from .models import db
+from flask_migrate import Migrate
+
 #define Flask object
 app = Flask(__name__, static_url_path = '/static', static_folder ='static')
 
@@ -11,5 +15,11 @@ app = Flask(__name__, static_url_path = '/static', static_folder ='static')
 #tell app how it should be configured -- on config.py file
 app.config.from_object(Config)
 
+#set up ORM and migrate connection
+db.init_app(app)
+migrate = Migrate(app, db)
+
+
 
 from . import routes
+from . import models
